32b90801a57bf2e377399f77dc1fd8ed31924db9
XxMeguminxX <saputraerik042@gmail.com>
2026-03-09 08:17:46 +0700

    new

---
+++ cek_angka.js
@@ -0,0 +1,6 @@
+let angka = 1;
+if (angka % 2 === 0){
+    console.log(`${angka} adalah angka genap`);
+} else {
+    console.log(`${angka} adalah angka ganjil`);
+}
---
+++ cek_true_false.js
@@ -0,0 +1,5 @@
+const cek = (angka) => {
+    return angka % 2 === 0
+}
+console.log(cek(2));
+console.log(cek(5));
